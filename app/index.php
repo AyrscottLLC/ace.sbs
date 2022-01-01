feec106050bb59e889599df060afc9f5fcd89d7a
Jared De Blander <jared@ayrscott.com>
2021-12-31 23:50:03 -0500

    minor updates

--- app/index.php
+++ app/index.php
@@ -4,11 +4,11 @@ require "../vendor/autoload.php";
 require "../lib/Mur.php";
 require "../lib/Discohook.php";
 
-Nanite::get('/', function(){
+Nanite::get('/', function () {
     Mur::render('primary');
 });
 
-Nanite::get('/category/([a-zA-Z0-9\-_]+)', function($category){
+Nanite::get('/category/([a-zA-Z0-9\-_]+)', function ($category) {
     Mur::render('category', array('category' => $category));
 });
 
@@ -18,18 +18,15 @@ Nanite::get('/category/([a-zA-Z0-9\-_]+)', function($category){
 // });
 
 // Regex enabled, groups get passed to the function.
-Nanite::get('/product/([a-zA-Z0-9\-_]+)', function($product){
+Nanite::get('/product/([a-zA-Z0-9\-_]+)', function ($product) {
     Mur::render('product', array('product' => $product));
 });
 
 // Handle a POST request
-Nanite::post('/contact', function(){
+Nanite::post('/contact', function () {
     // Handle submitted contact form.
     // var_dump($_POST);
     // $`
     $dh = new Discohook();
-    echo $dh->log(print_r($_POST, true));
-
-
+    $dh->log(print_r($_POST, true));
 });
-
