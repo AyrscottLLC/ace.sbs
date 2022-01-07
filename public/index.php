84b0d94d28981de95e510a97828b759a1f172277
Jared De Blander <jared@ayrscott.com>
2022-01-07 04:30:50 +0000

    minor updates

--- public/index.php
+++ public/index.php
@@ -3,21 +3,40 @@
 // set_include_path(get_include_path().';..\\vendor');
 // set_include_path('.;C:\Users\jared\OneDrive\Ayrscott\Projects\ace.sbs');
 
+
 require "../vendor/autoload.php";
+
 require "../lib/Mur.php";
 require "../lib/DH.php";
+
 require "../views/contact.php";
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->safeLoad();
 
 $data = array(
     'title' => 'Contact',
     'content' => 'Thank you for reaching out to us. Please feel free to contact us again at any time.'
 );
 
+// debug route
+
+Nanite::get('/debug', function () {
+    echo '<pre>';
+    var_dump($_ENV);
+    echo '</pre>';
+
+ });
+
+
+ // primary routes
 Nanite::get('/', function () { Mur::render('primary'); });
 Nanite::get('/category/([a-zA-Z0-9\-_]+)', function ($category) { Mur::render('category', array('category' => $category)); });
 Nanite::get('/product/([a-zA-Z0-9\-_]+)', function ($product) { Mur::render('product', array('product' => $product)); });
 Nanite::get('/cart', function () { Mur::render('cart'); });
 Nanite::post('/contact', function () { view_contact(); });
+
+// authenticated routes
 
 // Check if the user is logged in.
 if(isset($_SESSION['user'])) {
