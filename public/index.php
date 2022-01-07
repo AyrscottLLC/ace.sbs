2bf17eaa1a1cf7925ec7810401c2205c30cfaed5
Jared De Blander <jared@ayrscott.com>
2022-01-07 07:07:03 +0000

    minor updates

--- public/index.php
+++ public/index.php
@@ -20,7 +20,7 @@ Nanite::get('/', function () { Mur::render('primary'); });
 Nanite::get('/category/([a-zA-Z0-9\-_]+)', function ($category) { Mur::render('category', array('category' => $category)); });
 Nanite::get('/product/([a-zA-Z0-9\-_]+)', function ($product) { Mur::render('product', array('product' => $product)); });
 Nanite::get('/cart', function () { Mur::render('cart'); });
-Nanite::post('/contact', function () { view_contact(); });
+Nanite::post('/contact', function () { DH::logPost(); });
 
 // authenticated routes
 
